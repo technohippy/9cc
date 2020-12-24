51fb184e6f83af9beaae58d8e98ef24991b9ab82
ANDO Yasushi <yasushi.ando@kabuku.co.jp>
2020-12-25 00:00:36 +0900

    step6

--- 9cc.c
+++ 9cc.c
@@ -103,6 +103,7 @@ Node *new_node_num(int val) {
 }
 
 Node *mul();
+Node *unary();
 Node *primary();
 
 // expr = mul ("+" mul | "-" mul)*
@@ -119,18 +120,27 @@ Node *expr() {
   }
 }
 
-// mul = primary ("*" primary | "/" primary)*
+// mul = unary ("*" unary | "/" unary)*
 Node *mul() {
-  Node *node = primary();
+  Node *node = unary();
 
   for (;;) {
     if (consume('*'))
-      node = new_node(ND_MUL, node, primary());
+      node = new_node(ND_MUL, node, unary());
     else if (consume('/'))
-      node = new_node(ND_DIV, node, primary());
+      node = new_node(ND_DIV, node, unary());
     else
       return node;
   }
+}
+
+// unary   = ("+" | "-")? primary
+Node *unary() {
+  if (consume('+'))
+    return primary();
+  if (consume('-'))
+    return new_node(ND_SUB, new_node_num(0), primary());
+  return primary();
 }
 
 // primary = num | "(" expr ")"
